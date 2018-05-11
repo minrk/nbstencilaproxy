2c1c54089502ebdb6ad44d15d63259108a0ab141
Min RK <benjaminrk@gmail.com>
2018-05-11 14:57:17 +0100

    fix stencilaDist and enable logging

--- stencila.js
+++ stencila.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const darServer = require("dar-server");
 const express = require("express");
+const logging = require("morgan");
 
 const port = parseInt(process.env.STENCILA_PORT || '4000');
 const archiveDir = process.env.STENCILA_ARCHIVE_DIR || process.env.HOME;
@@ -15,7 +16,7 @@ darServer.serve(server, {
   apiUrl: "/archives"
 });
 
-node_modules = path.dirname(path.dirname(path.resolve(__dirname)));
+node_modules = path.dirname(path.resolve(__dirname));
 stencilaDist = path.join(node_modules, "stencila", "dist");
 
 console.log("Stencila app root: %s", stencilaDist);
@@ -23,6 +24,8 @@ console.log("DAR archive path: %s", archiveDir);
 console.log("DAR public URL: %s", serverUrl);
 console.log("Serving stencila on :%s", port);
 
+server.use(logging("dev"));
+console.log(stencilaDist)
 server.use("/stencilaDist", express.static(stencilaDist));
 server.use("/", express.static(__dirname));
 server.listen(port);
