562bb25684f94b23460a474d5f96d41ce298a97e
Min RK <benjaminrk@gmail.com>
2018-05-23 11:58:53 +0200

    support stencila installed top-level or as a pacakge
    location of stencila.js will change

--- nbstencilaproxy/handlers.py
+++ nbstencilaproxy/handlers.py
@@ -10,65 +10,67 @@ from notebook.base.handlers import IPythonHandler
 
 from nbserverproxy.handlers import SuperviseAndProxyHandler
 
+
 class AddSlashHandler(IPythonHandler):
     """Handler for adding trailing slash to URLs that need them"""
+
     @web.authenticated
     def get(self, *args):
         src = urlparse(self.request.uri)
-        dest = src._replace(path=src.path + '/')
+        dest = src._replace(path=src.path + "/")
         self.redirect(urlunparse(dest))
+
+
+def _find_stencila_js(name):
+    """Find a stencila.js file, whether it's top-level or in jupyter-dar"""
+    stencila_dir = os.environ["STENCILA_DIR"]
+    for sub_path in (name, os.path.join("node_modules", "jupyter-dar", name)):
+        stencila_js = os.path.join(stencila_dir, sub_path)
+        if os.path.exists(stencila_js):
+            return stencila_js
+    else:
+        raise FileNotFoundError("Could not find {} in {}".format(name, stencila_dir))
+
 
 # define our proxy handler for proxying the application
 
+
 class StencilaProxyHandler(SuperviseAndProxyHandler):
 
-    name = 'stencila'
+    name = "stencila"
 
     def get_env(self):
-        return {
-            'STENCILA_PORT': str(self.port),
-            'BASE_URL': self.state['base_url'],
-        }
+        return {"STENCILA_PORT": str(self.port), "BASE_URL": self.state["base_url"]}
 
     def get_cmd(self):
-        return [
-            "node",
-            os.environ['STENCILA_DIR'] + "/node_modules/jupyter-dar/stencila.js",
-        ]
+        return ["node", _find_stencila_js("stencila.js")]
 
 
 class StencilaHostProxyHandler(SuperviseAndProxyHandler):
 
-    name = 'stencila-host'
+    name = "stencila-host"
 
     def get_env(self):
-        return {
-            'STENCILA_HOST_PORT': str(self.port)
-        }
+        return {"STENCILA_HOST_PORT": str(self.port)}
 
     def get_cmd(self):
-        return [
-            "node",
-            os.environ['STENCILA_DIR'] + "/node_modules/jupyter-dar/stencila-host.js",
-        ]
+        return ["node", _find_stencila_js("stencila-host.js")]
 
 
 def setup_handlers(app):
-    app.web_app.add_handlers('.*', [
-        (
-            app.base_url + 'stencila/(.*)',
-            StencilaProxyHandler,
-            dict(state=dict(
-                base_url=app.base_url,
-                notebook_dir=app.notebook_dir,
-            )),
-
-        ), (
-            app.base_url + 'stencila-host/(.*)',
-            StencilaHostProxyHandler,
-            dict(state=dict(
-                base_url=app.base_url,
-                notebook_dir=app.notebook_dir,
-            )),
-        ),
-    ])
+    app.log.info("Enabling stencila proxy")
+    app.web_app.add_handlers(
+        ".*",
+        [
+            (
+                app.base_url + "stencila/(.*)",
+                StencilaProxyHandler,
+                dict(state=dict(base_url=app.base_url, notebook_dir=app.notebook_dir)),
+            ),
+            (
+                app.base_url + "stencila-host/(.*)",
+                StencilaHostProxyHandler,
+                dict(state=dict(base_url=app.base_url, notebook_dir=app.notebook_dir)),
+            ),
+        ],
+    )
