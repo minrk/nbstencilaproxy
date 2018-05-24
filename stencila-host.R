abee4328dbaf9e72a0af25f5f63f9631f6b814d9
nuest <daniel.nuest@uni-muenster.de>
2018-05-24 16:23:21 +0200

    Remove unused stencila-host.R

--- stencila-host.R
+++
@@ -1,9 +0,0 @@
-# Get the port passed here from the `nbserverproxy.hanglers.SuperviseAndProxyHandler`
-port <- as.integer(Sys.getenv("STENCILA_HOST_PORT"))
-if (is.na(port)) port <- 2000
-
-# Run the Stencila execution host without
-# any authentication (handled by Jupyter)
-Sys.setenv(STENCILA_AUTH = "false")
-
-stencila::run(port=port)
